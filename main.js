2d9f7e2115e71d14cb70dcc78acb364e1bbd597e
Karmele <ktelletxea.kt@gmail.com>
2024-03-07 10:29:29 +0100

    ontra manera

--- main.js
+++ main.js
@@ -3,15 +3,28 @@
   const botonIncrementar = document.getElementById('incrementar');
   const botonDecrementar = document.getElementById('decrementra');
   const valorContador = document.getElementById('valor');
+ 
+  function incrmentalo(){
+    contador++;
+    valorContador.textContent = contador;
+   }
+
+function decrementra(){
   
+    if (contador > 0 ){
+        contador--;
+        valorContador.textContent = contador;
+    } 
+    else ( contador < 0 ){
+
+    }
+}
+
+
   botonIncrementar.addEventListener('click', () => {
-      contador++;
-      valorContador.textContent = contador;
+      
   });
   
   botonDecrementar.addEventListener('click', () => {
-      if (contador > 0) {
-          contador--;
-          valorContador.textContent = contador;
-      }
+      
   });
