07b50a1438b90b2217da0e956caf886911843560
Karmele <ktelletxea.kt@gmail.com>
2024-03-07 10:22:59 +0100

    estoy muy perdida

--- main.js
+++ main.js
@@ -1,38 +1,17 @@
-// valor inicial : 0  nombre variable contar y el valor 0 ( variable let por que el resultado 
-//tendra cambios )
-let contar=0;
-//seleccion de valor y botones :
-// variable constante selecionamos el clas value 
-const value = document.querySelector("#value")
-//variable constante para selecionar botones:
-const botones = document.querySelector(".btn")
-// funcion de boton : 
+  let contador = 0;
 
-botones.forEach(function (btn) {
-    
-    btn.addEventListener("click", function (e) {
-      const styles = e.currentTarget.classList;
-//si en estinlos.contains disminuir contador a menos(--)
-      if (styles.contains("disminuir")) {
-        contar--;
-      } 
-// sino y si en el styles aumentar contador mas
-      else if (styles.contains("aumentar")) {
-        contar++;
-      } 
-      else {
-        contar = 0;
-      }
+  const botonIncrementar = document.getElementById('incrementar');
+  const botonDecrementar = document.getElementById('decrementra');
+  const valorContador = document.getElementById('valor');
   
-     /* if (count > 0) {
-        value.style.color = "green";
+  botonIncrementar.addEventListener('click', () => {
+      contador++;
+      valorContador.textContent = contador;
+  });
+  
+  botonDecrementar.addEventListener('click', () => {
+      if (contador > 0) {
+          contador--;
+          valorContador.textContent = contador;
       }
-      if (count < 0) {
-        value.style.color = "red";
-      }
-      if (count === 0) {
-        value.style.color = "#222";
-      }*/
-      value.textContent = count;
-    });
   });
